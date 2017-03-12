2ff0f0b59dc6e50f975d499288f844b58db7cb02
vuongdao <daoquangvuong@gmail.com>
2017-03-12 15:59:31 +0700

    nhap5

--- app/cate.php
+++ app/cate.php
@@ -60,7 +60,7 @@ require("templates/header_cate.php");
 			<div class="rest">
 				<div class="container margin-bottom">
 					<h1 class="left large margin-0">Chăm sóc da <span class="text-light-grey"> | </span><span class="small text-grey"> Tìm thấy 20 sản phẩm</span></h1>
-					<select name="" id="" class="right padding-4 container white round">
+					<select name="" id="" class="right padding-4 container white round hide-small">
 						<option value="0">Sắp xếp theo</option>
 						<option value="">Gía tăng dần</option>
 						<option value="">Gía giảm dần</option>
@@ -68,16 +68,16 @@ require("templates/header_cate.php");
 						<option value="">Bán chạy nhất</option>
 					</select>
 				</div>
-				<div class="cate-product row container">	
+				<div class="cate-product row">	
 
-					<div class="col l3 m3 s6 hover-shadow margin-bottom" >
-						<div class="product-thumb container margin-bottom">
+					<div class="col l3 m3 s6 hover-shadow" >
+						<div class="product-thumb padding">
 							<div class="product-image">
 								<a href="#"><img src="../public/images/product1.jpg" alt="" class="image"></a>
 							</div>
 							<div class="product-caption">
 								<div class="info">
-									<a href="#"><h4 class="small text-grey">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h4></a>
+									<a href="#"><h3 class="small">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h3></a>
 									<p>
 										<span class="text-red large">220,000đ</span>
 										<span class="text-grey"><del>150,000đ</del></span>
@@ -90,220 +90,220 @@ require("templates/header_cate.php");
 							</div>
 						</div>
 					</div>
-					<div class="col l3 m3 s6  hover-shadow margin-bottom" >
-						<div class="product-thumb container margin-bottom">
+					<div class="col l3 m3 s6  hover-shadow" >
+						<div class="product-thumb padding">
 							<div class="product-image">
 								<a href="#"><img src="../public/images/product2.jpg" alt="" class="image"></a>
 							</div>
 							<div class="product-caption">
 								<div class="info">
-									<a href="#"><h4 class="small text-grey">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h4></a>
+									<a href="#"><h3 class="small">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h3></a>
 									<p>
 										<span class="text-red large">220,000đ</span>
 										<span class="text-grey"><del>150,000đ</del></span>
 									</p>
 								</div>
-								<div class="cart">
+								<div class="cart hide-small">
 									<a href="#" class="btn-block red">Mua hàng</a>
 								</div>
 								
 							</div>
 						</div>
 					</div>
-					<div class="col l3 m3 s6  hover-shadow margin-bottom" >
-						<div class="product-thumb container margin-bottom">
+					<div class="col l3 m3 s6  hover-shadow" >
+						<div class="product-thumb padding">
 							<div class="product-image">
 								<a href="#"><img src="../public/images/product3.jpg" alt="" class="image"></a>
 							</div>
 							<div class="product-caption">
 								<div class="info">
-									<a href="#"><h4 class="small text-grey">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h4></a>
+									<a href="#"><h3 class="small">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h3></a>
 									<p>
 										<span class="text-red large">220,000đ</span>
 										<span class="text-grey"><del>150,000đ</del></span>
 									</p>
 								</div>
-								<div class="cart">
+								<div class="cart hide-small">
 									<a href="#" class="btn-block red">Mua hàng</a>
 								</div>
 								
 							</div>
 						</div>
 					</div>
-					<div class="col l3 m3 s6  hover-shadow margin-bottom" >
-						<div class="product-thumb container margin-bottom">
+					<div class="col l3 m3 s6  hover-shadow" >
+						<div class="product-thumb padding">
 							<div class="product-image">
 								<a href="#"><img src="../public/images/product4.jpg" alt="" class="image"></a>
 							</div>
 							<div class="product-caption">
 								<div class="info">
-									<a href="#"><h4 class="small text-grey">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h4></a>
+									<a href="#"><h3 class="small">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h3></a>
 									<p>
 										<span class="text-red large">220,000đ</span>
 										<span class="text-grey"><del>150,000đ</del></span>
 									</p>
 								</div>
-								<div class="cart">
+								<div class="cart hide-small">
 									<a href="#" class="btn-block red">Mua hàng</a>
 								</div>
 								
 							</div>
 						</div>
 					</div>
-					<div class="col l3 m3 s6  hover-shadow margin-bottom" >
-						<div class="product-thumb container margin-bottom">
+					<div class="col l3 m3 s6  hover-shadow" >
+						<div class="product-thumb padding">
 							<div class="product-image">
 								<a href="#"><img src="../public/images/product1.jpg" alt="" class="image"></a>
 							</div>
 							<div class="product-caption">
 								<div class="info">
-									<a href="#"><h4 class="small text-grey">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h4></a>
+									<a href="#"><h3 class="small">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h3></a>
 									<p>
 										<span class="text-red large">220,000đ</span>
 										<span class="text-grey"><del>150,000đ</del></span>
 									</p>
 								</div>
-								<div class="cart">
+								<div class="cart hide-small">
 									<a href="#" class="btn-block red">Mua hàng</a>
 								</div>
 								
 							</div>
 						</div>
 					</div>
-					<div class="col l3 m3 s6  hover-shadow margin-bottom" >
-						<div class="product-thumb container margin-bottom">
+					<div class="col l3 m3 s6  hover-shadow" >
+						<div class="product-thumb padding">
 							<div class="product-image">
 								<a href="#"><img src="../public/images/product2.jpg" alt="" class="image"></a>
 							</div>
 							<div class="product-caption">
 								<div class="info">
-									<a href="#"><h4 class="small text-grey">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h4></a>
+									<a href="#"><h3 class="small">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h3></a>
 									<p>
 										<span class="text-red large">220,000đ</span>
 										<span class="text-grey"><del>150,000đ</del></span>
 									</p>
 								</div>
-								<div class="cart">
+								<div class="cart hide-small">
 									<a href="#" class="btn-block red">Mua hàng</a>
 								</div>
 								
 							</div>
 						</div>
 					</div>
-					<div class="col l3 m3 s6  hover-shadow margin-bottom" >
-						<div class="product-thumb container margin-bottom">
+					<div class="col l3 m3 s6  hover-shadow" >
+						<div class="product-thumb padding">
 							<div class="product-image">
 								<a href="#"><img src="../public/images/product3.jpg" alt="" class="image"></a>
 							</div>
 							<div class="product-caption">
 								<div class="info">
-									<a href="#"><h4 class="small text-grey">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h4></a>
+									<a href="#"><h3 class="small">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h3></a>
 									<p>
 										<span class="text-red large">220,000đ</span>
 										<span class="text-grey"><del>150,000đ</del></span>
 									</p>
 								</div>
-								<div class="cart">
+								<div class="cart hide-small">
 									<a href="#" class="btn-block red">Mua hàng</a>
 								</div>
 								
 							</div>
 						</div>
 					</div>
-					<div class="col l3 m3 s6  hover-shadow margin-bottom" >
-						<div class="product-thumb container margin-bottom">
+					<div class="col l3 m3 s6  hover-shadow" >
+						<div class="product-thumb padding">
 							<div class="product-image">
 								<a href="#"><img src="../public/images/product4.jpg" alt="" class="image"></a>
 							</div>
 							<div class="product-caption">
 								<div class="info">
-									<a href="#"><h4 class="small text-grey">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h4></a>
+									<a href="#"><h3 class="small">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h3></a>
 									<p>
 										<span class="text-red large">220,000đ</span>
 										<span class="text-grey"><del>150,000đ</del></span>
 									</p>
 								</div>
-								<div class="cart">
+								<div class="cart hide-small">
 									<a href="#" class="btn-block red">Mua hàng</a>
 								</div>
 								
 							</div>
 						</div>
 					</div>
-					<div class="col l3 m3 s6  hover-shadow margin-bottom" >
-						<div class="product-thumb container margin-bottom">
+					<div class="col l3 m3 s6  hover-shadow" >
+						<div class="product-thumb padding">
 							<div class="product-image">
 								<a href="#"><img src="../public/images/product5.jpg" alt="" class="image"></a>
 							</div>
 							<div class="product-caption">
 								<div class="info">
-									<a href="#"><h4 class="small text-grey">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h4></a>
+									<a href="#"><h3 class="small">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h3></a>
 									<p>
 										<span class="text-red large">220,000đ</span>
 										<span class="text-grey"><del>150,000đ</del></span>
 									</p>
 								</div>
-								<div class="cart">
+								<div class="cart hide-small">
 									<a href="#" class="btn-block red">Mua hàng</a>
 								</div>
 								
 							</div>
 						</div>
 					</div>
-					<div class="col l3 m3 s6  hover-shadow margin-bottom" >
-						<div class="product-thumb container margin-bottom">
+					<div class="col l3 m3 s6  hover-shadow" >
+						<div class="product-thumb padding">
 							<div class="product-image">
 								<a href="#"><img src="../public/images/product6.jpg" alt="" class="image"></a>
 							</div>
 							<div class="product-caption">
 								<div class="info">
-									<a href="#"><h4 class="small text-grey">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h4></a>
+									<a href="#"><h3 class="small">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h3></a>
 									<p>
 										<span class="text-red large">220,000đ</span>
 										<span class="text-grey"><del>150,000đ</del></span>
 									</p>
 								</div>
-								<div class="cart">
+								<div class="cart hide-small">
 									<a href="#" class="btn-block red">Mua hàng</a>
 								</div>
 								
 							</div>
 						</div>
 					</div>
-					<div class="col l3 m3 s6  hover-shadow margin-bottom" >
-						<div class="product-thumb container margin-bottom">
+					<div class="col l3 m3 s6  hover-shadow" >
+						<div class="product-thumb padding">
 							<div class="product-image">
 								<a href="#"><img src="../public/images/product7.jpg" alt="" class="image"></a>
 							</div>
 							<div class="product-caption">
 								<div class="info">
-									<a href="#"><h4 class="small text-grey">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h4></a>
+									<a href="#"><h3 class="small">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h3></a>
 									<p>
 										<span class="text-red large">220,000đ</span>
 										<span class="text-grey"><del>150,000đ</del></span>
 									</p>
 								</div>
-								<div class="cart">
+								<div class="cart hide-small">
 									<a href="#" class="btn-block red">Mua hàng</a>
 								</div>
 								
 							</div>
 						</div>
 					</div>
-					<div class="col l3 m3 s6  hover-shadow margin-bottom" >
-						<div class="product-thumb container margin-bottom">
+					<div class="col l3 m3 s6  hover-shadow" >
+						<div class="product-thumb padding">
 							<div class="product-image">
 								<a href="#"><img src="../public/images/product8.jpg" alt="" class="image"></a>
 							</div>
 							<div class="product-caption">
 								<div class="info">
-									<a href="#"><h4 class="small text-grey">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h4></a>
+									<a href="#"><h3 class="small">Sữa tắm cao cấp Happy Bath Hàn Quốc Chai 500ml + Túi 250ml</h3></a>
 									<p>
 										<span class="text-red large">220,000đ</span>
 										<span class="text-grey"><del>150,000đ</del></span>
 									</p>
 								</div>
-								<div class="cart">
+								<div class="cart hide-small">
 									<a href="#" class="btn-block red">Mua hàng</a>
 								</div>
 								
@@ -326,5 +326,11 @@ require("templates/header_cate.php");
 			  <a href="#" class="button">&raquo;</a>
 			</div>
 		</div>
-	
+		
+		<div class="filter-bar bar light-grey center">
+			<a style="width:33.33%" href="#" class="bar-item border-right">Danh mục</a>
+			<a style="width:33.33%" href="#" class="bar-item border-right">Thương hiệu</a>
+		 	<a style="width:33.33%" href="#" class="bar-item">Sắp xếp</a>
+		</div>
+
 </section>
